a0e6caeaf6b025f8b6ead68b40c0920ecf1250c0
rasha08 <rasha08@gmail.com>
2017-10-26 08:38:52 +0200

    fixed problem with getting error from server as a response

--- src/app/services/articles-counter-http.service.ts
+++ src/app/services/articles-counter-http.service.ts
@@ -1,23 +1,22 @@
-import { Injectable } from "@angular/core";
+import { Injectable, NgZone } from "@angular/core";
 import { Http } from "@angular/http";
 
 
 @Injectable()
 
 export class ArticleCounterHTTPService {
-    public BASE_URL = 'https://fitneszona.rs/';  
+    public BASE_URL = 'https://fitneszona.rs';  
 
     constructor(
-        private _http: Http
-    ){ }
+        private _http: Http,
+        private _ngZone: NgZone
+    ) {}
 
-    
-    getCategoriesWithNewArticles(timestring){
-        console.log(timestring);
-        return this._http.get(`${this.BASE_URL}/api/articles/all/counter`, timestring).map(
+    getCategoriesWithNewArticles(timestring) {
+        const body = JSON.stringify({timestring});
+        return this._http.post(`${this.BASE_URL}/api/articles/all/counter`, body).map(
             response => response.json(),
-            error => console.log('Errorat articles-counter-http.service:',error)
+            error => this._ngZone.runOutsideAngular(() => setTimeout(() => this.getCategoriesWithNewArticles(timestring), 500))
         );
     }
-
 }
--- src/app/services/configuration-http.service.ts
+++ src/app/services/configuration-http.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@angular/core";
+import { Injectable, NgZone } from "@angular/core";
 import { Http, RequestOptions } from "@angular/http";
 import { Subject } from "rxjs/Subject";
 
@@ -11,17 +11,23 @@ export class ConfigurationHTTPService {
     public BASE_URL: string = 'https://fitneszona.rs';
     
     constructor(
-        private _http: Http
-    ){ }
+        private _http: Http,
+        private _ngZone: NgZone
+    ) {}
 
-    public getActiveCategories(){
+    public getActiveCategories() {
         return this._http.get(`https://fitneszona.rs/api/configuration/1/categories`)
-            .map(response => response.json())
+            .map(
+                response => response.json(),
+                error => this._ngZone.runOutsideAngular(() => setTimeout(() => this.getActiveCategories(), 500))
+            );
     }
 
-    public getUserConfiguration(id){
+    public getUserConfiguration(id) {
         return this._http.get(`${this.BASE_URL}/api/configuration/${id}`)
-            .map(response => response.json()) 
+            .map(
+                response => response.json(),
+                error => this._ngZone.runOutsideAngular(() => setTimeout(() => this.getUserConfiguration(id), 500))
+            );
     }
-
 }
