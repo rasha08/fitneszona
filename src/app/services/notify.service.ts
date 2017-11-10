2bb17d539f38ff2ac1439cf0a6bfb3f5ff961b9d
rasha08 <rasha08@gmail.com>
2017-11-10 17:31:01 +0100

    uncoment notify

--- src/app/services/notify.service.ts
+++ src/app/services/notify.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-//import { database } from 'firebase';
+import { database } from 'firebase';
 
 @Injectable()
 export class NotifyService {
   public subscribeToConfigurationChanges(id) {
-    //return database().ref(`/configurationsfb/${id}`);
+    return database().ref(`/configurationsfb/${id}`);
   }
 }
