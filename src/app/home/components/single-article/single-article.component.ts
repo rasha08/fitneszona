92dc9c151a7b389cdec2687224641607af622a46
rasha08 <rasha08@gmail.com>
2017-11-12 14:35:40 +0100

    implemented basic design structure for articles

--- src/app/home/components/single-article/single-article.component.ts
+++ src/app/home/components/single-article/single-article.component.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
+declare const $;
 
 @Component({
   selector: 'single-article',
   templateUrl: './single-article.html'
 })
-export class SingleArticleComponent {}
+export class SingleArticleComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    $(document).ready(function(){
+      $('.tooltipped').tooltip({delay: 50});
+    });
+  }
+}
